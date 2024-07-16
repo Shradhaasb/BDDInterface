79beb7af1e455111225f53529a0156fb933ab7cd
HP <HP@DESKTOP-E29FVJN>
2024-07-16 15:01:30 +0530

    create a HandledropDown

---
+++ src/main/java/UtilityLayer/Utils.java
@@ -0,0 +1,12 @@
+package utilityLayer;
+
+public class Utils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Utils class created");
+
+	}
+
+}
---
+++ src/main/java/UtilityLayer/handledropDown.java
@@ -0,0 +1,10 @@
+package UtilityLayer;
+
+public class handledropDown {
+
+	public static void main(String[] args) {
+		System.out.println("xyz");
+
+	}
+
+}
